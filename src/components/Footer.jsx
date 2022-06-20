434ed812ad740a5c44bb513465c5ee2fa3107337
Aden Smith <adensmithy13@gmail.com>
2022-06-20 16:46:16 -0700

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
                     <Link to="/" className="footer__link">Home</Link>
                     <span className="footer__link no-cursor">About</span>
                     <Link to="/books" className="footer__link">Books</Link>
-                    <Link to="/car" className="footer__link">Cart</Link>
+                    <Link to="/cart" className="footer__link">Cart</Link>
                 </div>
                 <div className="footer__copyright">
                     Copyright &copy; 2021 Library
